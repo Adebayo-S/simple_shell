77a7f2599a5e052c232fd6eaf383ad64e0fd7253
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-13 12:37:06 +0100

    [feat #10]: parse_input function created

--- string_parser.c
+++ string_parser.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 #include "shell.h"
+
+char **parse_input(char *args)
+{
+	char *delimiter = " ";
+	char **output = malloc((_strlen(args) + 1) * sizeof(char *));
+	if (output == NULL)
+		return (NULL);
+
+	char* token = _strtok(args, delimiter);
+	int i = 0;
+
+	while (token != NULL)
+	{
+		output[i] = token;
+		i++;
+		token = strtok(NULL, delimiter);
+	}
+	output[i] = NULL;
+
+	return (output);
+}
