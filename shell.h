b764e2a6c4974602ba54f289c88744ad6158f2fd
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-14 22:28:30 +0100

    [fix]: header updated

--- shell.h
+++ shell.h
@@ -5,16 +5,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+
+#define EXEC 0
 
 typedef struct cmd_t
 {
-	int *mode;
+	int mode;
 	char **args;
 } cmd_t;
 
-void prompt(void);
-char **parse_input(char *args);
+void prompt(int status);
+void t_error(char *s);
+int _fork(void);
+char **get_toks(char *args, char *delimiter);
 void setcmd(char *buf, cmd_t *cmd);
 void runcmd(char **input, cmd_t *cmd);
 
