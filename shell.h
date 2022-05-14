978077c57e76c0bff51b892e0a023f66f056dc1f
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-14 01:29:28 +0100

    [fix #8]: header file updated

--- shell.h
+++ shell.h
@@ -1,7 +1,21 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+typedef struct cmd_t
+{
+	int *mode;
+	char **args;
+} cmd_t;
+
 void prompt(void);
-char **parse_input(char *buffer);
+char **parse_input(char *args);
+void setcmd(char *buf, cmd_t *cmd);
+void runcmd(char **input, cmd_t *cmd);
 
 #endif // SHELL_H
