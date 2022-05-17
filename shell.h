81acf8669e926a0d940cc5957baccb5853e1a19f
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-17 22:00:05 +0100

    [fix]: header updated

--- shell.h
+++ shell.h
@@ -12,22 +12,36 @@
 
 #define EXEC 1
 #define MAXARGS 10
+#define DELIM " \t\n\r\a"
+
+extern char **environ;
 
 typedef struct cmd_t
 {
 	int mode;
 	char **args;
 	int ready;
+	// char **_environ;
 } cmd_t;
 
+void open_console(void);
 void init_cmd(cmd_t *cmd);
 void prompt(int status);
 void t_error(char *s);
 int _fork(void);
 char **get_toks(char *args, char *delimiter);
 void setcmd(char *buf, cmd_t *cmd);
-void runcmd(char **input, cmd_t *cmd);
+void runcmd(char* dir, char **input, cmd_t *cmd);
 char *_getline(void);
 
+/* builtins */
+typedef struct builtins
+{
+	char *name;
+	int (*f)(void);
+} built_t;
 
-#endif // SHELL_H
+int parse_builtins(char **input);
+
+
+#endif /* SHELL_H */
