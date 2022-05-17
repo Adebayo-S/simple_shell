e909da2ce4ff32c8a2bbfc0d0b14d61fe3d34d79
moscode <odunoshomoses@gmail.com>
2022-05-17 11:14:50 +0000

    [fix #16] _getline restructure to take in arguments

--- _getline.c
+++ _getline.c
@@ -31,3 +31,75 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 		free(buffer);
 	}
 }
+
+/**
+ * _getline - Reads input from a stream.
+ * @lineptr: A buffer to store the input.
+ * @n: The size of lineptr.
+ * @stream: The stream to read from.
+ *
+ * Return: The number of bytes read.
+ */
+ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+{
+    static ssize_t input;
+    ssize_t ret;
+    char c = 'x';
+    char *buffer;
+    int r;
+    
+    if (input == 0)
+    {
+        fflush(stream);
+    }
+    else
+    {
+        return (-1);
+    }
+    
+    input = 0;
+    
+    //Allocating memory
+    buffer = malloc(sizeof(char) * 120);
+    
+    // if memory allocation fails
+    if (!buffer)
+    {
+        return (-1);
+    }
+    
+    //Initialize a loop to read all char if c is not newline
+    while (c != '\n')
+    {
+        r = read(STDIN_FILENO, &c, 1);
+        if (r == -1 || (r == 0 && input == 0))
+        {
+            free(buffer);
+            return (-1);
+        }
+        if (r == 0 && input != 0)
+        {
+            input++;
+            break;
+        }
+        if (input >= 120)
+        {
+            buffer = realloc(buffer, input, input + 1);
+        }
+        
+        buffer[input] = c;
+        input++
+    }
+    
+    //add newline at the end of the buffer
+    buffer[input] = '\n';
+    
+    assign_lineptr(lineptr, n, buffer, input);
+    
+    ret = input;
+    if(r != 0)
+    {
+        input = 0;
+    }
+    return (ret);
+}
