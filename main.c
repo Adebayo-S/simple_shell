7a29d7c0f3826e17b484c4d99155b57c08f9d648
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-18 16:38:01 +0100

    [fix]: used _getline and implemented minor style changes

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of the shell
+ *
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: the (int)value of status.
+ */
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
 	char *buf, *dir;
 	struct stat st;
 	cmd_t cmd;
-	int fd, status;
+	int status;
 	size_t buflen = 0;
 	pid_t id;
 	char **input;
@@ -22,24 +30,26 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 		status = isatty(STDIN_FILENO);
 		prompt(status);
 
-		if (getline(&buf, &buflen, stdin) <= EOF)
+		if (_getline(&buf, &buflen, stdin) <= EOF)
 			cmd.ready = 0, exit(EXIT_SUCCESS);
 
 		setcmd(buf, &cmd);
 		input = get_toks(buf, DELIM);
 
-		dir = _which(input[0]);
-
-		if (parse_builtins(dir))
+		if (parse_builtins(input, &cmd))
 			continue;
+
+		dir = _which(input[0]);
 
 		if (dir && _fork() == 0)
 			runcmd(dir, input, &cmd);
+		else if (!dir)
+			t_error("invalid command\n");
 		else
 			wait(NULL);
 	}
 
 	free(buf);
 	free(input);
-	return (0);
+	return (cmd.status);
 }
