14d6bdda5a78302a04d37f6a4e596206cb8b13bf
moscode <odunoshomoses@gmail.com>
2022-05-18 12:03:52 +0000

    [fix]: update shell.h

--- shell.h
+++ shell.h
@@ -19,6 +19,7 @@ void setcmd(char *buf, cmd_t *cmd);
 void runcmd(char **input, cmd_t *cmd);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src)
 
 
 #endif // SHELL_H
