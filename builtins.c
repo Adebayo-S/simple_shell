41cdd6e47c01319dd98600bfe187689acc1165be
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-18 16:34:44 +0100

    [fix]: reset cmd->ready on exit

--- builtins.c
+++ builtins.c
@@ -1,32 +1,6 @@
 #include "shell.h"
 
 /**
- * _env - prints the evironment variables
- *
- * @input: input data from getline.
- * @cmd: struct of global variables
- * Return: EXIT_SUCCESS on success.
- */
-int _env(char **input, cmd_t *cmd)
-{
-	char **tmp = input;
-	tmp = NULL;
-
-	int i, j;
-
-	for (i = 0; environ[i]; i++)
-	{
-		for (j = 0; environ[i][j]; j++)
-			;
-
-		write(STDOUT_FILENO, environ[i], j);
-		write(STDOUT_FILENO, "\n", 1);
-	}
-
-	exit(EXIT_SUCCESS);
-}
-
-/**
  * exit_sh - exits the shell
  *
  * @input: vector of inputed arguments
@@ -42,17 +16,18 @@ int exit_sh(char **input, cmd_t *cmd)
 
 	if (input[1])
 	{
-		ustatus = _atoi(input[1]);
+		ustatus = atoi(input[1]);
 		is_digit = _isdigit(input[1]);
-		str_len = _strlen(input[1]);
+		str_len = strlen(input[1]);
 		big_number = ustatus > (unsigned int)INT_MAX;
 		if (!is_digit || str_len > 10 || big_number)
 		{
 			t_error("Invalid exit status");
-			cmd->ready = 0;
 			cmd->status = 2;
 		}
 		cmd->status = (ustatus % 256);
+		cmd->ready = 0;
 	}
-	exit(EXIT_SUCCESS);
+	cmd->ready = 0;
+	return (1);
 }
