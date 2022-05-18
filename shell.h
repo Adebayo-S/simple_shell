6d938fc7ac2416eee2f3e390be23e3c54b850e7d
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-18 16:41:34 +0100

    [fix #8]: updated header

--- shell.h
+++ shell.h
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <sys/wait.h>
 
 #define EXEC 1
@@ -21,7 +22,7 @@ typedef struct cmd_t
 	int mode;
 	char **args;
 	int ready;
-	// char **_environ;
+	int status;
 } cmd_t;
 
 void open_console(void);
@@ -29,19 +30,35 @@ void init_cmd(cmd_t *cmd);
 void prompt(int status);
 void t_error(char *s);
 int _fork(void);
-char **get_toks(char *args, char *delimiter);
 void setcmd(char *buf, cmd_t *cmd);
 void runcmd(char* dir, char **input, cmd_t *cmd);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
-/* builtins */
+/* ------------------ENVIRONMENT----------------- */
+char *_which(char *input);
+int _env(char **input);
+char *_getenv(const char *name);
+
+/* ------------------BUILTINS----------------- */
 typedef struct builtins
 {
 	char *name;
-	int (*f)(void);
+	int (*f)(char **input);
 } built_t;
 
-int parse_builtins(char **input);
+int parse_builtins(char **input, cmd_t *cmd);
+int exit_sh(char **input, cmd_t *cmd);
+int c_dir(char **input);
+/*---change directory----*/
+int cd_path(char *dir);
+int cd_parent(void);
+int cd_curr(void);
+int cd_back(void);
+int cd_home(void);
 
+/* ------------------STRING PARSER----------------- */
+int _isdigit(const char *str);
+char **get_toks(char *args, char *delimiter);
+void str_reverse(char *s);
 
 #endif /* SHELL_H */
