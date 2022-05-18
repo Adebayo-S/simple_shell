c187234ac8958b69053998840c0eaf72f4fc55cf
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-19 00:37:33 +0100

    [style]: cleanup for check 1

--- builtins.c
+++ builtins.c
@@ -32,24 +32,4 @@ int exit_sh(char **input, cmd_t *cmd)
 	return (1);
 }
 
-/**
- * c_dir - change directory master function
- * @input: the input string
- */
-/*
- * int c_dir(char **input)
- * {
- * char *args = input[1];
- * if (!args || !_strcmp(args, "~") || !_strcmp(args, "$HOME") ||
- * !_strcmp(args, "--"))
- * return (cd_home());
- * else if (!_strcmp(args, "-"))
- * return (cd_back());
- * lse if (!_strcmp(args, "."))
- * return (cd_curr());
- * else if (!_strcmp(args, ".."))
- * return (cd_parent());
- * else
- * return (cd_path(input[1]));
- * }
- */
+
