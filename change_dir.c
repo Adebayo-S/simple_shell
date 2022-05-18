13c5c4c169210709414cd82146f27e1b422dbbaa
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-18 01:13:55 +0100

    [feat]: cd_back, cd_curr & cd_parent change dir functions created

--- change_dir.c
+++ change_dir.c
@@ -1,3 +1,4 @@
+#include "shell.h"
 
 /*lsca*/
 int c_dir(char **input)
@@ -12,4 +13,86 @@ int c_dir(char **input)
 	if (!strcmp(args, ".."))
 		cd_parent();
 	cd_path();
+}
+
+/**
+ * cd_back - changes to the previous directory
+ *
+ * Return: EXIT_SUCCESS
+ */
+int cd_back(void)
+{
+	char cwd[PATH_MAX];
+	char *p_cwd, *p_oldcwd, *cp_cwd, *cp_oldcwd;
+
+	getcwd(cwd, sizeof(cwd));
+	cp_cwd = strdup(cwd);
+
+	p_oldcwd = _getenv("OLDPWD");
+
+	if (p_oldcwd == NULL)
+		cp_oldcwd = cp_cwd;
+	else
+		cp_oldcwd = strdup(p_oldcwd);
+
+	setenv("OLDPWD", cp_cwd);
+
+	if (chdir(cp_oldcwd) == -1)
+		setenv("PWD", cp_cwd);
+	else
+		setenv("PWD", cp_oldcwd);
+
+	p_cwd = _getenv("PWD");
+
+	write(STDOUT_FILENO, p_cwd, _strlen(p_cwd));
+	write(STDOUT_FILENO, "\n", 1);
+
+	free(cp_cwd);
+	if (p_oldcwd)
+		free(cp_oldcwd);
+
+	chdir(p_cwd);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * cd_curr - changes to the current directory
+ *
+ * Return: EXIT_SUCCESS
+ */
+int cd_curr(void)
+{
+	char cwd[PATH_MAX];
+	char *cp_cwd;
+
+	getcwd(cwd, sizeof(cwd));
+	cp_cwd = strdup(cwd);
+
+	setenv("PWD", cp_cwd);
+	free(cp_cwd);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * cd_parent - changes to the current directory
+ *
+ * Return: EXIT_SUCCESS
+ */
+int cd_parent(void)
+{
+	char cwd[PATH_MAX];
+	char *cp_cwd;
+
+	getcwd(cwd, sizeof(cwd));
+	cp_cwd = strdup(cwd);
+
+	if (!strcmp("/", cp_cwd))
+	{
+		free(cp_cwd);
+		exit(EXIT_SUCCESS);
+	}
+
+	setenv("OLDPWD", cp_cwd);
+	free(cp_cwd);
+	exit(EXIT_SUCCESS);
 }
