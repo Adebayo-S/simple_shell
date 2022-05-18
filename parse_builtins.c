1b93e887fb2f3c4114167526318338f8731b5759
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-18 02:37:22 +0100

    [fix]: seperated the exit command from the builtin array

--- parse_builtins.c
+++ parse_builtins.c
@@ -1,22 +1,22 @@
 #include "shell.h"
 
-int parse_builtins(char **input)
+int parse_builtins(char **input, cmd_t *cmd)
 {
 	int i;
 
 	built_t builtin[] = {
-		{"cd", c_dir}, { "env", _env }, { "exit", exit_sh },
-		{ "setenv", _setenv }, { "unsetenv", _unsetenv },
-		{ "help", _help }, { NULL, NULL }
+		{"cd", c_dir}, {"env", _env}, {"setenv", _setenv},
+		{"unsetenv", _unsetenv}, {"help", _help},
+		{NULL, NULL}
 	};
+
+	if (!strcmp("exit", input[0]))
+		exit_sh(input[0], cmd);
 
 	for (i = 0; builtin[i].f; i++)
 	{
 		if (!strcmp(builtin[i].name, input[0]))
-		{
 			builtin[i].f(input);
-			exit(EXIT_SUCCESS);
-		}
 	}
 	exit(EXIT_FAILURE);
 }
