cd3b51e489ef9253bd33c0c4797fa8dd8781cd0c
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-19 00:20:25 +0100

    [style]: cleanup for check 1

--- shell.h
+++ shell.h
@@ -44,8 +44,6 @@ void *_realloc(void *ptr, size_t old_size, size_t new_size);
 void runcmd(char* dir, char **input, cmd_t *cmd);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_strtok(char *str, const char *delim);
-int _setenv(list_t **env, char **str);
-int _unsetenv(list_t **env, char **str);
 
 /* ------------------ENVIRONMENT----------------- */
 char *_which(char *input);
