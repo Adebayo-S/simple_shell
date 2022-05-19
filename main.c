39800857607818b4421f50cfcecb7b16282ebfc1
Adebayo Samuel <eladebayoor@gmail.com>
2022-05-19 01:37:42 +0100

    [fix]: line 31 STDIN

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 		status = isatty(STDIN_FILENO);
 		prompt(status);
 
-		if (_getline(&buf, &buflen, stdin) <= EOF)
+		if (_getline(&buf, &buflen, STDIN_FILENO) <= EOF)
 			cmd.ready = 0, exit(EXIT_SUCCESS);
 
 		setcmd(buf, &cmd);
