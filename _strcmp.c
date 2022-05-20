a0dd42c05ded38e9ab748580a72512f1c98629f7
Moscode <odunoshomoses@gmail.com>
2022-05-19 17:14:43 -0700

    [fix #11]: refactor _strcmp

--- _strcmp.c
+++ _strcmp.c
@@ -11,10 +11,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+	//Checking for null and then comparing
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
+		return (s1[i] - s2[i]);
 }
