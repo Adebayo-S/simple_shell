44b3f9ec6520ff00b7a40af80d7d97ddac21feeb
Moscode <odunoshomoses@gmail.com>
2022-05-19 18:05:28 -0700

    [fix]: refactored _strcat

--- _strcat.c
+++ _strcat.c
@@ -3,20 +3,19 @@
  * *_strcat - concatenates two strings
  * @dest: pointer destination
  * @src: pointer source
- * Return: void
+ * Return: pointer to string dest
 */
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
+int a = 0, i;
+
 for (i = 0; dest[i] != '\0'; i++)
-;
-
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
-
+	;
+while (src[a] != '\0')
+{
+	dest[a + i] = src[a];
+	a++
+}
 return (dest);
 }
